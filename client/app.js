6fc9be5aa9f99953d3f8c3cffecd0ca4e6b10c7e
phaedraa <phaedra.a.r@gmail.com>
2017-04-23 13:53:38 -0700

    Add AppBar

--- client/app.js
+++ client/app.js
@@ -1,6 +1,31 @@
-import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 import FacebookLogin from 'react-facebook-login';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import React, { Component } from 'react';
 import statusChangeCallback from './statusChangeCallback';
+import Toggle from 'material-ui/Toggle';
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Page 1" />
+    <MenuItem primaryText="Page 2" />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
 
 class App extends Component {
   checkLoginState() {
@@ -10,16 +35,26 @@ class App extends Component {
   }
 
   render() {
+    console.log('props', this.props);
+
     return (
-      <div className="App">
-        <FacebookLogin
-          appId="167040673631579"
-          autoLoad={true}
-          callback={this.checkLoginState}
-          fields="name,email,picture"
-          cssClass="my-facebook-button"
-        ></FacebookLogin>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <AppBar
+            title="Title"
+            iconElementLeft={<Logged />}
+            iconElementRight={<IconButton><NavigationClose /></IconButton>}
+          />
+          <FacebookLogin
+            appId="167040673631579"
+            autoLoad={true}
+            callback={this.checkLoginState}
+            fields="name,email,picture"
+            cssClass="my-facebook-button"
+          ></FacebookLogin>
+        </div>
+      </MuiThemeProvider>
+
     );
   }
 }
