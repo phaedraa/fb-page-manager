bd16b4fd1dea472495ea3dc68bc277ba5050df8d
Phaedra <phaedra.a.r@gmail.com>
2017-04-22 23:07:03 -0700

    Move function to be class function

--- client/app.js
+++ client/app.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import FacebookLogin from 'react-facebook-login';
 import statusChangeCallback from './statusChangeCallback';
 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
-
 class App extends Component {
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <FacebookLogin
           appId="167040673631579"
           autoLoad={true}
-          callback={checkLoginState}
+          callback={this.checkLoginState}
           fields="name,email,picture"
           cssClass="my-facebook-button"
         ></FacebookLogin>
