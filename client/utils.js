158250271b19e558065249f70a5f95d4058d20e1
Phaedra <phaedra.a.r@gmail.com>
2017-04-22 18:34:07 -0700

    Setup utilities file

--- client/utils.js
+++ client/utils.js
@@ -1,40 +1,36 @@
-function utils () {}
-
-utils.getPagesData(responseData) = function {
-  pagesData = response.data;
-    for (var j = 0; j < pagesData.length; j++) {
-    pagesData[j].posts = getPagePosts(pagesData[j].id);
-  }
-  return pagesData;
-}
-
-utils.getPagePosts(pageID) = function {
-  var posts = {unpublished: [], published: []};
-  FB.api(
-    '/' + pageID + '/posts?fields=message,created_time,is_published',
-    function (response) {
-      var data = response.data;
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].is_published) {
-          posts.published.push(data[i]);
-        } else {
-          posts.unpublished.push(data[i]);
+export default {
+  getPagesData: function(responseData) {
+    pagesData = response.data;
+      for (var j = 0; j < pagesData.length; j++) {
+      pagesData[j].posts = getPagePosts(pagesData[j].id);
+    }
+    return pagesData;
+  },
+  getPagePosts: function(pageID) {
+    var posts = {unpublished: [], published: []};
+    FB.api(
+      '/' + pageID + '/posts?fields=message,created_time,is_published',
+      function (response) {
+        var data = response.data;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].is_published) {
+            posts.published.push(data[i]);
+          } else {
+            posts.unpublished.push(data[i]);
+          }
         }
       }
-    }
-  );
-  return posts;
-}
-
-utils.publishPost(pageID, data) = function {
-  FB.api(
-    "/" + pageID + "/feed",
-    'post',
-    data,
-    function (response) {
-      console.log('published: ', response);
-    }
-  );
-}
-
-module.exports = utils;
+    );
+    return posts;
+  },
+  publishPost: function(pageID, data) {
+    FB.api(
+      '/' + pageID + '/feed',
+      'post',
+      data,
+      function (response) {
+        console.log('published: ', response);
+      }
+    );
+  }
+};
