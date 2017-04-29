1a37501ba3ed3cf67a56daf214d23c1d159d4d8d
Phaedra <phaedra.a.r@gmail.com>
2017-04-29 16:12:22 -0700

    Extract page info card to a component

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import AppBar from 'material-ui/AppBar';
 import BarMenu from './BarMenu';
-import {Card, CardActions, CardHeader, CardText, CardTitle} from 'material-ui/Card';
 import FacebookLogin from 'react-facebook-login';
 import IconMenu from 'material-ui/IconMenu';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -11,13 +10,13 @@ import React, { Component } from 'react';
 import Subheader from 'material-ui/Subheader';
 import Toggle from 'material-ui/Toggle';
 import utils from './utils';
+import PageInfoCard from './PageInfoCard';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.checkLoginState = this.checkLoginState.bind(this);
     this.userInfoReceived = this.userInfoReceived.bind(this);
-    this.pageInfoReceived = this.pageInfoReceived.bind(this);
     this.userPagesDataReceived = this.userPagesDataReceived.bind(this);
     this.setPageID = this.setPageID.bind(this);
     this.state = { isLoggedIn: false };
@@ -74,51 +73,6 @@ class App extends Component {
     }
   }
 
-  getWelcomeText() {
-    var welcomeMessage = 'In the FB Page Manager App, you can post' +
-      ' to your pages, review insights on your published posts, and preview' +
-      ' your unpublished posts. ';
-    return (
-      <div>
-        {welcomeMessage}
-        <b>Log in to your Facebook below to get started!</b>
-      </div>
-    );
-  }
-
-  pageInfoReceived(data) {
-    this.setState({
-      pagePhotoURL: data.url || '',
-      pageAbout: data.about || '',
-      pageName: data.name || ''
-    });
-  }
-
-  getPageInfo() {
-    if (!this.state.pageID) {
-      return (<b>Select a page with which to work.</b>);
-    }
-
-    utils.getBasicPageInfo(this.state.pageID, this.pageInfoReceived);
-    return (
-      <Card>
-        <CardHeader
-          avatar={this.state.pagePhotoURL}
-          title={this.state.pageName}
-          subtitle={this.state.pageAbout}
-        />
-      </Card>
-    );
-  }
-
-  getPageInfoCard() {
-    if (this.state.isLoggedIn) {
-      return this.getPageInfo();
-    }
-
-    return <Subheader>{this.getWelcomeText()}</Subheader>
-  }
-
   getPageNavBar() {
     if (this.state.isLoggedIn && this.state.pageID) {
       return <PageTabs pageID={this.state.pageID}/>
@@ -143,6 +97,8 @@ class App extends Component {
   }
 
   render() {
+    const { isLoggedIn, pageID } = this.state;
+
     return (
       <MuiThemeProvider>
         <div className="App">
@@ -150,7 +106,7 @@ class App extends Component {
             title={this.getTitle()}
             iconElementLeft={this.getMenu()}
           />
-          {this.getPageInfoCard()}
+          <PageInfoCard isLoggedIn={isLoggedIn} pageID={pageID} />
           <div>{this.getPageNavBar()}</div>
           <div>{this.getFBLoginButton()}</div>
         </div>
